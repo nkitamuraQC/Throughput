c93af8fcd961536f2afe7bc9d018dd0328e7c495
Naoki Kitamura <qclove00@NaokinoMacBook-Pro.local>
2024-11-02 16:09:12 +0900

    auto_detection

--- throughput/determine_window_from_dos.py
+++ throughput/determine_window_from_dos.py
@@ -44,41 +44,25 @@ def search_window(A):
     return idx, overall_max_j
 
 
-def cumulative_sum(A):
-    # 配列 A の長さを取得
-    n = len(A)
-    
-    # 累積和のリスト S を作成。S[0] は 0 とし、S[i] は A[0] から A[i-1] までの合計。
-    S = [0] * (n + 1)
-    
-    # 累積和の計算
-    for i in range(n):
-        S[i + 1] = S[i] + A[i]
-    
-    return S
-
-
-def read_dos(dosfile, choice1=2, choice2=5):
+def read_dos(dosfile, target_start=2, target_end=7):
     rf = open(dosfile, "r")
     lines = rf.readlines()
     e_ret = []
     dos_ret = []
     integral = 0.0
     for i in range(1,len(lines)):
-        e = float(lines[i].split()[0]) - fermi
+        e = float(lines[i].split()[0])
         _ = lines[i].split()[1]
-        dos_ = lines[i].split()[choice1:choice2]
+        dos_ = lines[i].split()[target_start: target_end]
         val = 0.0
         for i in range(len(dos_)):
             val += float(dos_[i])
-        tmp = []
-        if e > range_[0] and e < range_[1]:
-            e_ret.append(e)
-            dos_ret.append(val)
+        e_ret.append(e)
+        dos_ret.append(val)
     integral += sum(dos_ret)
     return e_ret, dos_ret, integral
 
-def det_single_dos(dosfile):
+def for_single_dos(dosfile):
     energy, dos, _ = read_dos(dosfile)
     a, b = search_window(dos)
     start = energy[a]
@@ -94,5 +78,16 @@ def convert_to_fortran_notation(num):
     # 'e'を'd'に置き換えたFortran形式に変換
     fortran_notation = sci_notation.replace('e', 'd')
     
-    return fortran_notation
+    # 指数部が0の場合にd0を付加する
+    if 'd0' in fortran_notation:
+        return fortran_notation  # 既に d0 形式ならそのまま返す
+    elif 'd+0' in fortran_notation or 'd-0' in fortran_notation:
+        fortran_notation = fortran_notation.replace('d+0', 'd0').replace('d-0', 'd0')
     
+    return fortran_notation
+
+if __name__ == "__main__":
+    fdos = "throughput/calculation.pdos_atm#1(Fe)_wfc#4(d)"
+
+    start, end = for_single_dos(fdos)
+    print(start, end)
